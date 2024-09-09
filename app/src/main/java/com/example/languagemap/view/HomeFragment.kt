071546f251da62f49cd65521a8d3f34241a3f06c
umut <diler.2029@gmail.com>
2024-09-09 09:56:49 +0300

    State secured between screens

--- app/src/main/java/com/example/languagemap/view/HomeFragment.kt
+++ app/src/main/java/com/example/languagemap/view/HomeFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.languagemap.R
 import com.example.languagemap.adapter.HomeAdapter
 import com.example.languagemap.data.allWords
 import com.example.languagemap.databinding.FragmentHomeBinding
+import com.example.languagemap.model.Items
+import com.example.languagemap.viewmodel.HomeViewModel
+import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: HomeViewModel
+    private var shuffledList: List<Items> = listOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,10 +35,22 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.countyRecyclerView.adapter  = HomeAdapter(allWords)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        observeData()
 
         binding.swipeRefreshLayout.setOnRefreshListener {
+            viewModel.shuflleItems()
+            observeData()
             binding.swipeRefreshLayout.isRefreshing = false
+        }
+    }
+
+    fun observeData(){
+        viewModel.shuffleItemsForOnce()
+        lifecycleScope.launch {
+            viewModel.itemsState.collect {
+                binding.countyRecyclerView.adapter = HomeAdapter(it)
+            }
         }
     }
 
--- app/src/main/java/com/example/languagemap/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/languagemap/viewmodel/HomeViewModel.kt
@@ -1,9 +1,30 @@
 package com.example.languagemap.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.languagemap.data.allWords
+import com.example.languagemap.model.Items
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class HomeViewModel: ViewModel() {
 
+    private var _itemsState = MutableStateFlow(listOf<Items>())
+    val itemsState get() = _itemsState
+    private var isShuffled = false
+
+    fun shuffleItemsForOnce(){
+        if (!isShuffled) {
+            _itemsState.update{
+                allWords.shuffled()
+            }
+            isShuffled = true
+        }
+    }
+
+    fun shuflleItems(){
+        _itemsState.update{
+            allWords.shuffled()
+        }
+    }
 
 }
